43c3b540cfd496d03b7c445ee17f641f4db5bdcd
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-07 12:03:03 -0500

    Began question prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,32 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project? (Required)",
+      // validation.
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your project title!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please include a description of your project.",
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +35,5 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
+questions();
 init();
