e1db56ae55b2b26bb700d6d105c3eac167a9d209
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-07 19:38:14 -0500

    Saves the user input to the template variable and pushes that to the new renderReadme function to generate the ReadMe.MD file in the Dist SubFolder

--- index.js
+++ index.js
@@ -1,83 +1,99 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMarkdown = require("./utils/generateMarkdown.js");
+// const generateMarkdown = require("./utils/generateMarkdown.js");
+
+// ReadMe Template that gets generated based on the user input.
+const renderReadme = (input) => {
+  return `
+  # ${input.title}
+
+  ## Project Description
+  ${input.description}
+
+  `;
+};
 
 // TODO: Create an array of questions for user input
-
-const questionsArray = [
-  {
-    type: "input",
-    name: "title",
-    message: "What is the title of your project? (Required)",
-    // validation.
-    validate: (nameInput) => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log("Please enter your project title!");
-        return false;
-      }
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project? (Required)",
+      // validation.
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your project title!");
+          return false;
+        }
+      },
     },
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Please include a description of your project.",
-  },
-
-  {
-    type: "input",
-    name: "githubURL",
-    message: "What is the gitHubURL for your project? (Required)",
-    validate: (nameInput) => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log("Please enter the gitHub URL for your project!");
-        return false;
-      }
+    {
+      type: "input",
+      name: "description",
+      message: "Please include a description of your project.",
     },
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Enter your email address:",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message:
-      "Please provide a step-by-step description of to install your project. (Required)",
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Please choose a license from the provided list.",
-    choices: ["MIT", "GPL", "APACHE", "BOOST"],
-  },
-  {
-    type: "input",
-    name: "contribution",
-    message:
-      "Are their any contribution requirements/restrictions for your project?",
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: "Please advise on how to test your application:",
-  },
-];
-//Used inquirer to capture the responses from the questions.
-inquirer.prompt(questionsArray).then((responses) => {
-  console.log(responses.title);
-  //created const variable fileName to the utils dir for the generateMarkdown js file
-  const fileName = "";
-});
+
+    {
+      type: "input",
+      name: "githubURL",
+      message: "What is the gitHubURL for your project? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter the gitHub URL for your project!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message:
+        "Please provide a step-by-step description of to install your project. (Required)",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Please choose a license from the provided list.",
+      choices: ["MIT", "GPL", "APACHE", "BOOST"],
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message:
+        "Are their any contribution requirements/restrictions for your project?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Please advise on how to test your application:",
+    },
+  ])
+  // captures data from the user input
+  .then(function (data) {
+    // saves the input as a variable and pushes it to the renderReadme function.
+    const template = renderReadme(data);
+    //writes the template variable (renderReadme(Data)) as a ReadMe.MD file and saves it to the Dist subfolder
+    fs.writeFile("./Dist/ReadMe.MD", template, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Success!!!");
+    });
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
 function init() {}
 
